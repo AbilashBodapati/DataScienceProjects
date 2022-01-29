fe42ee52e33c7fb3bbd3147c966d6d429d268777
Abilash Bodapati <bodapatiabilash98@gmail.com>
2022-01-29 15:13:56 -0500

    Finished sample app

--- uber_pickups.py
+++ uber_pickups.py
@@ -7,6 +7,8 @@ st.title('Uber pickups in NYC')
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+
+# Caching the Data
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -15,7 +17,31 @@ def load_data(nrows):
     data['date/time'] = pd.to_datetime(data['date/time'])
     return data
 
+def print_raw_data(data):
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
+
+def plot_pickups_by_hour(data):
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(data['date/time'].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
+
+def custom_filter_slider(data):
+    # st.header('Map of all pickups')
+    # st.map(data)
+    hour_to_filter = st.slider('hour', 0, 23, 17)
+    filtered_data = data[data['date/time'].dt.hour == hour_to_filter]
+    st.subheader('Map of all pickups at {}:00'.format(hour_to_filter))
+    st.map(filtered_data)
+
 if __name__ == '__main__':
     data_load_state = st.text('Loading Data...')
     data = load_data(10000)
     data_load_state.text('Loading Data...done!')
+
+    print_raw_data(data)
+
+    plot_pickups_by_hour(data)
+
+    custom_filter_slider(data)
